0382bd716b20418d5d393ae82e1474e092ca51ca
Peter van der Leek <petervdleek@gmail.com>
2026-05-11 18:54:23 +0200

    Adjust health check with existence of specific image

--- html/health-check.php
+++ html/health-check.php
@@ -1,8 +1,12 @@
 <?php
 
 if (\file_exists('stop.txt')) {
-    header('HTTP/1.1 503 Service Unavailable');
+    \header('HTTP/1.1 503 Service Unavailable');
 } else {
-    header('HTTP/1.1 200 OK');
+    if (\file_exists('/var/www/bnls_2026/IMG-0095-69bf9fd7a2ea6.jpg')) {
+        \header('HTTP/1.1 200 OK');
+    } else {
+        \header('HTTP/1.1 503 Service Unavailable');
+    }
 }
 echo '<meta http-equiv="refresh" content="5">';
